de859e38ada33c3c1348c10ef717cb02896ca0ea
amerorchis <andrew.p.smith95@gmail.com>
2023-04-14 15:15:31 -0600

    change app routing

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 # Define a route that returns a string on a GET request
-@app.route('/drip')
+@app.route('/')
 def return_events():
     test_date = datetime(2023, 7, 4)
     response = {'events': nas_events(test_date)}
