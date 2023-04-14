43d7e1ef1e706079bdc801b13a897d1c61fd74e1
amerorchis <andrew.p.smith95@gmail.com>
2023-04-14 15:39:55 -0600

    add a second app route

--- api/index.py
+++ api/index.py
@@ -5,10 +5,14 @@ from datetime import datetime
 # Create a Flask app instance
 app = Flask(__name__)
 
-# Define a route that returns a string on a GET request
-@app.route('/', methods=['GET'])
+# Define a route that returns a JSON on a GET request to /drip
+@app.route('/drip', methods=['GET'])
 def return_events():
     test_date = datetime(2023, 7, 4)
     response = {'events': nas_events(test_date)}
-
     return jsonify(response)
+
+# Define a route that returns a string on a GET request
+@app.route('/', methods=['GET'])
+def return_events():
+    return nas_events(datetime(2023, 7, 4))
