a3d839e89ba74e7f1162d593beb1809bacc09042
amerorchis <andrew.p.smith95@gmail.com>
2023-04-17 11:50:29 -0600

    Add routing to handle URIs

--- dripAPI/__main__.py
+++ dripAPI/__main__.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from api.nas import nas_events
+from flask import Flask, jsonify, request
+from dripAPI.nas import nas_events
 from datetime import datetime
 
 # Create a Flask app instance
@@ -8,8 +8,14 @@ app = Flask(__name__)
 # Define a route that returns a JSON on a GET request to /drip
 @app.route('/drip', methods=['GET'])
 def return_events():
-    test_date = datetime(2023, 7, 4)
-    response = {'events': nas_events(test_date)}
+    
+    # Check if Drip is in test mode.
+    test_mode = False
+    if request.args: 
+        test_mode = True if 'preview' in request.args and request.args['preview'] == 'true' else False
+    day = datetime(2023, 7, 4) if test_mode else datetime.now()
+    
+    response = {'events': nas_events(day)}
     return jsonify(response)
 
 # Define a route that returns a string on a GET request
